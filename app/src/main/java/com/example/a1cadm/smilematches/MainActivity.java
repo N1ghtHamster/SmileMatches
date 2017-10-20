65e61615acf3e858d8d8cd7240d4a26c6bd3ede2
1cadm <111@exxx.ru>
2017-10-20 10:40:19 +0300

    ch MA

--- app/src/main/java/com/example/a1cadm/smilematches/MainActivity.java
+++ app/src/main/java/com/example/a1cadm/smilematches/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.a1cadm.smilematches;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button bnGame;
+    Button bStatistic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        bnGame = (Button) findViewById(R.id.bnGame);
+        bStatistic = (Button) findViewById(R.id.bStatistic);
+
+    }
+    // Метод обработки нажатия на кнопку
+    public void onClick(View v) {
+        Intent intent = new Intent(a);
+        // по id определеяем кнопку, вызвавшую этот обработчик
+        switch (v.getId()) {
+            case R.id.bnGame:
+                // кнопка ОК
+                tvOut.setText("Нажата кнопка ОК");
+                break;
+            case R.id.bStatistic:
+                // кнопка Cancel
+                tvOut.setText("Нажата кнопка Cancel");
+                break;
+        }
     }
 }
+
